0c20177f39bb77918a8b5836664c2f1f9083149b
Wei Wu <>
2020-04-27 18:53:50 -0400

    Testing version
    initial upload

---
+++ File Crypter/File Crypter/Decrypter.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace File_Crypter
+{
+    class Decrypter
+    {
+
+        public static string IV = "brk38bBeD6oyi3rp"; //16
+        public static string key = "fmeldmhlrp3lg;6lgkrm7,3lg;oi7m3;"; //32
+
+        public static void DecrpytFolder(string input, string output)
+        {
+
+            string inputFile = input;
+            string outputFile = output;
+
+            var aes = new AesCryptoServiceProvider();
+            aes.BlockSize = 128;
+            aes.KeySize = 256;
+            aes.Key = ASCIIEncoding.ASCII.GetBytes(key);
+            aes.IV = ASCIIEncoding.ASCII.GetBytes(IV);
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Mode = CipherMode.CBC;
+            ICryptoTransform icrypt = aes.CreateEncryptor(aes.Key, aes.IV);
+            ICryptoTransform dcrypt = aes.CreateDecryptor(aes.Key, aes.IV);
+
+          
+
+
+            string[] files2 = Directory.GetFiles(input);
+
+            foreach (var f in files2)
+            {
+
+                FileInfo fi = new FileInfo(f);
+                using (FileStream fsOut = System.IO.File.OpenWrite(output + (@"\") + fi.Name))
+                {
+                    using (CryptoStream stream = new CryptoStream(fsOut, dcrypt, CryptoStreamMode.Write))
+                    {
+                        using (FileStream fsIn = System.IO.File.OpenRead(f))
+                        {
+
+                            //read in ~1 MB  chunks
+                            int bufferLen = 1000000;
+                            byte[] buffer = new byte[bufferLen];
+                            int bytesRead;
+                            do
+                            {
+                                bytesRead = fsIn.Read(buffer, 0, buffer.Length);
+                                stream.Write(buffer, 0, bytesRead);
+                            } while (bytesRead != 0);
+                        }
+                    }
+                }
+
+
+
+            }
+
+        }
+
+    }
+}
---
+++ File Crypter/File Crypter/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Crypter
+{
+    public partial class Form1 : Form
+    {
+
+        private string _encryptSource;
+        private string _encryptDestination;
+        private string _decryptSource;
+        private string _decryptDestination;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Properties.Settings.Default.encryptSource;
+            textBox2.Text = Properties.Settings.Default.encryptDestination;
+            textBox3.Text = Properties.Settings.Default.decryptSource;
+            textBox4.Text = Properties.Settings.Default.decryptDestination;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Encrypt Button
+            Encrypter.EncryptFolder(_encryptSource, _encryptDestination);
+            label3.Text = "Completed Encryption";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Decrpyt Button
+            Decrypter.DecrpytFolder(_decryptSource, _decryptDestination);
+            label3.Text =  "Completed Decryption";
+        }
+
+
+        public string ChooseFolder()
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                return folderBrowserDialog1.SelectedPath;
+            }
+            else {
+                return "Not Valid";
+            }
+        }
+
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _encryptSource = ChooseFolder();
+            textBox1.Text = _encryptSource;
+            Properties.Settings.Default.encryptSource = _encryptSource;
+            Properties.Settings.Default.Save();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            _encryptDestination = ChooseFolder();
+            textBox2.Text = _encryptDestination;
+            Properties.Settings.Default.encryptDestination = _encryptDestination;
+            Properties.Settings.Default.Save();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            _encryptSource = textBox1.Text;
+            Properties.Settings.Default.encryptSource = _encryptSource;
+            Properties.Settings.Default.Save();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            _encryptDestination = textBox2.Text;
+            Properties.Settings.Default.encryptDestination = _encryptDestination;
+            Properties.Settings.Default.Save();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            _decryptSource = ChooseFolder();
+            textBox3.Text = _decryptSource;
+            Properties.Settings.Default.decryptSource = _decryptSource;
+            Properties.Settings.Default.Save();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            _decryptDestination = ChooseFolder();
+            textBox4.Text = _decryptDestination;
+            Properties.Settings.Default.decryptDestination = _decryptDestination;
+            Properties.Settings.Default.Save();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+            _decryptSource = textBox3.Text;
+            Properties.Settings.Default.decryptSource = _decryptSource;
+            Properties.Settings.Default.Save();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            _decryptDestination = textBox4.Text;
+            Properties.Settings.Default.decryptDestination = _decryptDestination;
+            Properties.Settings.Default.Save();
+        }
+
+
+    }
+}
